40c9b13d12ab1de4cdf406fd17cc5beec021c75a
Denperidge <cat@denperidge.com>
2026-06-10 15:48:29 +0200

    tests: reformat test_rsync

--- development/test_rsync.py
+++ development/test_rsync.py
@@ -1,6 +1,9 @@
 from shared import eco
 
 EXPECTED_a = "--archive, -a            archive mode is -rlptgoD (no -A,-X,-U,-N,-H)\n"
+def test_a():
+    assert eco("rsync -a") == EXPECTED_a
+
 EXPECTED_rlptgoD = EXPECTED_a + \
 """--recursive, -r          recurse into directories
 --links, -l              copy symlinks as symlinks
@@ -10,14 +13,9 @@ EXPECTED_rlptgoD = EXPECTED_a + \
 --owner, -o              preserve owner (super-user only)
 -D                       same as --devices --specials
 """
-EXPECTED_chmod = "--chmod=CHMOD            affect file and/or directory permissions\n"
-
-
-def test_a():
-    assert eco("rsync -a") == EXPECTED_a
-
 def test_rlptgoD():
     assert eco("rsync -rlptgoD") == EXPECTED_rlptgoD
     
+EXPECTED_chmod = "--chmod=CHMOD            affect file and/or directory permissions\n"
 def test_chmod():
     assert eco("rsync --chmod") == EXPECTED_chmod
