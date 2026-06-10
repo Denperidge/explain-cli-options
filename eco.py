d76ff5b9d801e7cb246e20ca4b085f9479a7e0f9
Denperidge <cat@denperidge.com>
2026-06-10 18:01:04 +0200

    eco: debug, improved extra line detection

--- eco.py
+++ eco.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 from subprocess import run as _run
 from argparse import ArgumentParser
-from re import search, MULTILINE
+from re import search, MULTILINE, IGNORECASE
+
+debug = lambda x: x
 
 def run(command: str, capture_output=True) -> str:
     out = _run(command, shell=True, encoding="utf-8", capture_output=capture_output)
@@ -14,19 +16,30 @@ def get_help(command: str) -> list[str]:
     return helpvar.split("\n")
 
 def search_help(helplines: list[str], needle: str) -> list[str]:
+    regex_find_needle = fr"([^-\w]|^){needle}([^-]|$)"
+    regex_letters_not_From_needle = fr"[^\W{needle.replace("-", "")}]"
+    debug(f"Searching help for {needle} with {regex_find_needle} & {regex_letters_not_From_needle}")
+    
     relevant_lines = []
     i = 0  # while instead of for so index can be increased
     while i != len(helplines):
         line = helplines[i]
         # If regex can find needle in line
-        if search(pattern=fr"([^-\w]|^){needle}([^-]|$)", flags=MULTILINE, string=line):
+        if search(pattern=regex_find_needle, string=line, flags=MULTILINE):
+            debug(f"\t[MATCH {needle}] {line}")
             # Return it
             relevant_lines.append(line)
-            # If the line is empty aside from needle
-            if line.strip() == needle:
+            
+            debug(f"\t[MATCH {needle}] checking if only {needle} letters are in line")
+            # If the line only has word characters that are in the needle
+            if not search(pattern=regex_letters_not_From_needle, string=line, flags=IGNORECASE):
+                debug(f"\t[EMPTY] no letters aside from {needle} letters are in line. Adding extra line...")
+                
                 i += 1  # Move index up
                 relevant_lines.append(helplines[i])  # Add next line
-                
+        else:
+            debug(f"\t[NON MATCH {needle}] {line}")
+
         # If the needle is in the line but not detected by regex, warn
         #elif needle in line:
         #    print(f"Possibly relevant, but not detected by regex:\n{line}")
@@ -58,11 +71,15 @@ if __name__ == "__main__":
     parser = ArgumentParser(
         description="eco is a no-further-dependency python script that explains any cli options you throw at it <3 (hopefully!)",
         prefix_chars="+")
+    parser.add_argument("++debug", "+d", action="store_true", help="aaa")
     parser.add_argument("command", help="command which needs explaining (for example, tar)")
     parser.add_argument("args", nargs="+", help="args for the command you want explained (for example, -cvzf)")
 
     args = parser.parse_args()
-
+    
+    if args.debug:
+        debug = lambda x: print(x)
+    
     for line in parse_help(args.command, args.args):
         print(line)
 
