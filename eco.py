e42322bc567852573576e5c8b4ce524f7fed339c
Denperidge <cat@denperidge.com>
2026-06-10 19:35:29 +0200

    eco: first draft get_man & renames
    - run raises ChildProcessError
    - parse_help -> get_relevant_command_docs
    - search_help -> search_in_lines

--- eco.py
+++ eco.py
@@ -8,22 +8,29 @@ debug = lambda x: x
 def run(command: str, capture_output=True) -> str:
     out = _run(command, shell=True, encoding="utf-8", capture_output=capture_output)
     if out.returncode != 0:
-        raise Exception(out.stderr)
+        raise ChildProcessError(out.stderr)
     return out.stdout
 
 def get_help(command: str) -> list[str]:
     helpvar = run(f"{command} --help")
     return helpvar.split("\n")
 
-def search_help(helplines: list[str], needle: str) -> list[str]:
+def get_man(command: str) -> list[str]:
+    try:
+        return run(f"man {command}").split("\n")
+    except ChildProcessError:
+        debug("Could not run man")
+        return False
+
+def search_in_lines(haystack: list[str], needle: str) -> list[str]:
     regex_find_needle = fr"([^-\w]|^){needle}([^-]|$)"
     regex_letters_not_From_needle = fr"[^\W{needle.replace("-", "")}]"
     debug(f"Searching help for {needle} with {regex_find_needle} & {regex_letters_not_From_needle}")
     
     relevant_lines = []
     i = 0  # while instead of for so index can be increased
-    while i != len(helplines):
-        line = helplines[i]
+    while i != len(haystack):
+        line = haystack[i]
         # If regex can find needle in line
         if search(pattern=regex_find_needle, string=line, flags=MULTILINE):
             debug(f"\t[MATCH {needle}] {line}")
@@ -36,7 +43,7 @@ def search_help(helplines: list[str], needle: str) -> list[str]:
                 debug(f"\t[EMPTY] no letters aside from {needle} letters are in line. Adding extra line...")
                 
                 i += 1  # Move index up
-                relevant_lines.append(helplines[i])  # Add next line
+                relevant_lines.append(haystack[i])  # Add next line
         else:
             debug(f"\t[NON MATCH {needle}] {line}")
 
@@ -47,21 +54,21 @@ def search_help(helplines: list[str], needle: str) -> list[str]:
 
     return relevant_lines
 
-def parse_help(command: str, command_args:list[str]) -> list[str]:
-    helpvar = get_help(command)
+def get_relevant_command_docs(command: str, command_args:list[str]) -> list[str]:
+    lines = get_help(command) # get_man(command) or get_help(command)
     to_print = []
     for arg in command_args:
         # e.g. --progress or status
         if arg.startswith("--") or not arg.startswith("-"):
-            to_print += search_help(helpvar, arg)
+            to_print += search_in_lines(lines, arg)
         elif arg.startswith("-"):
             if len(arg) == 2:  # e.g. -d
-                to_print += search_help(helpvar, arg)
+                to_print += search_in_lines(lines, arg)
             else:  # e.g. -xvz
                 needles = list(arg)
                 needles.remove("-")
                 for needle in needles:
-                    to_print += search_help(helpvar, f"-{needle}")
+                    to_print += search_in_lines(lines, f"-{needle}")
         else:
             print(f"Skipping non-option: {arg}")
     return to_print
@@ -76,11 +83,11 @@ if __name__ == "__main__":
     parser.add_argument("args", nargs="+", help="args for the command you want explained (for example, -cvzf)")
 
     args = parser.parse_args()
-    
+
     if args.debug:
         debug = lambda x: print(x)
     
-    for line in parse_help(args.command, args.args):
+    for line in get_relevant_command_docs(args.command, args.args):
         print(line)
 
 
