b481b760d25f430e96ca1f370b3b4c3e70c56edf
Denperidge <cat@denperidge.com>
2026-06-10 23:06:45 +0200

    test_deps man cargo, ci apt cargo

--- development/test_deps.py
+++ development/test_deps.py
@@ -4,3 +4,7 @@ import pytest
 @pytest.mark.parametrize("command", ["uv", "cargo", "man", "rsync", "tar"])
 def test_command_exists(command: str):
     assert run(command + " --help", shell=True).returncode == 0
+
+@pytest.mark.parametrize("command", ["cargo"])
+def test_man_exists(command: str):
+    assert run("man " + command, shell=True).returncode == 0
