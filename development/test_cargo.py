5c0547c559585ca49566066b4f52d93c17d89037
Denperidge <cat@denperidge.com>
2026-06-12 00:45:50 +0200

    tests: disable docker-comp, begin cargo add dev

--- development/test_cargo.py
+++ development/test_cargo.py
@@ -1,4 +1,4 @@
-from testutils import eco
+from testutils import eco, more_found
 
 EXPECTED_v = "  -v, --verbose...               Use verbose output (-vv very verbose/build.rs output)\n"
 def test_v():
@@ -12,3 +12,15 @@ def test_pkgid():
     # The ubuntu-latest CI runner has 2 extra spaces on the output here
     assert eco("cargo pkgid") == EXPECTED_pkgid or\
         eco("cargo pkgid") == EXPECTED_pkgid.replace("c", "  c", count=1).replace("P", "  P", count=1)
+
+COMMAND_add_dev = "'cargo add' --dev"
+EXPECTED_add_dev = """      --dev
+          Add as development dependency
+"""
+def test_add_dev_with_quotes():
+    # This is originally the only way to get cargo add help info
+    assert eco(COMMAND_add_dev) == more_found(EXPECTED_add_dev, COMMAND_add_dev.replace("'", ""), man=True) 
+
+# This is the behaviour after patching
+#def test_add_dev_without_quotes():
+#    assert eco(COMMAND_add_dev.replace("'", "")) == EXPECTED_add_dev
